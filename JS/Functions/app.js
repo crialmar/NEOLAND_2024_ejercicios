4386c359659f5fcd1aaa1faeb5ea1908bb0b43a2
crialmar <cristina.almagon@gmail.com>
2024-01-11 17:02:28 +0100

    Iteration 2

---
+++ JS/Functions/app.js
@@ -0,0 +1,28 @@
+//?----------- Iteración #1: Buscar el máximo
+//! Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function sum(numberOne , numberTwo) {
+  if (numberOne > numberTwo) {
+    return numberOne
+  } else {
+    return numberTwo
+  }
+}
+let mayorNum = sum(8,1);
+console.log(mayorNum)
+
+//?----------- Iteración #2: Buscar la palabra más larga
+//!Completa la función que tomando un array de strings como argumento devuelva el más largo,
+//!en caso de que dos strings tenga la misma longitud deberá devolver el primero.
+
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+function findLongestWord(param) {
+  // insert code
+
+}
+
+//?----------- Iteración #3: Calcular la suma
+
+
+//?----------- Iteración #4: Calcular el promedio
+
