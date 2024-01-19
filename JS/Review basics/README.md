115a53096c67486b8428de74c82ff23c9d28e622
crialmar <cristina.almagon@gmail.com>
2024-01-19 18:22:53 +0100

    RB 3

--- JS/Review basics/README.md
+++ JS/Review basics/README.md
@@ -85,7 +85,7 @@ function findArrayIndex(array, text) {}
 Ej array:
 ['Caracol', 'Mosquito', 'Salamandra', 'Ajolote']
 ​
-Iteración #5: Función rollDice
+**Iteración #5: Función rollDice**
 Crea una función llamada rollDice() que reciba como parametro el numero de caras que queramos que tenga el dado que deberá silumar el codigo dentro de la función. Como hemos dicho, que la función use el parametro para simular una tirada de dado y retornar el resultado. Si no se te ocurre como hacer un numero aleatorio no te preocupes! busca información sobre la función de javascript Math.random();
 Iteración #6: Función swap
 Crea una función llamada swap() que reciba un array y dos parametros que sean indices del array. La función deberá intercambiar la posición de los valores de los indices que hayamos enviado como parametro. Retorna el array resultante.
--- JS/Review basics/app.js
+++ JS/Review basics/app.js
@@ -10,20 +10,20 @@ const movies = [
     {title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación']},
     ]//*------------> categorías son comedia, aventura, accion, thriller y animación
 
-let categoria = []
-const categoriasVuelta = (movie, movies) => {
-    let acc = 0
-    for (movie of movies) {
-    if(movie.categories.includes('comedia', 'aventura', 'acción', 'thiller', 'animación')) {
-        acc++
+let categoriaLista = []
+
+for (let movie of movies) {//-------> Primer paso: entrar en el array. Buscamos las películas dentro de Movies
+    for(let category of movie.categories){//---------> Segundo paso: entrar en el objeto. Buscamos la categoría dentro de cada movie
+        if(!categoriaLista.includes(category)) { //---------> Tercer paso: hay que meter las categorías sin repetir dentro de categoriaLista. Para ello, empezamos 
+        categoriaLista.push(category)//---------------------> negando el if para qu entienda que si NO hay esa categoría la lance dentro del array vacío
     }
-    categoria.push(movie.categories)
-}
+    }
 }
 
-console.log("🚀 ~ categoria:", categoria)
+console.log("🚀 ~ categoriaLista:", categoriaLista)
 
-//?----------- Iteración #1: Mix Fors
+
+//?----------- Iteración #2: Mix Fors
 //! Dado el siguiente javascript usa forof y forin para hacer la media del volumen de todos los sonidos favoritos que tienen los usuarios.
 
 const users = [
@@ -57,12 +57,100 @@ const users = [
     },
     ]
 
-    let claveSonido = []
-
-    for (let clave of users){
-        if(clave.includes("favoritesSounds")){
-            claveSonido.push(clave)
-        }
+/*let usuarios = []
+    
+for (let user of users){
+    if(!usuarios.includes(user)){
+    usuarios.push(user)
     }
 
-    console.log("🚀 ~ claveSonido:", claveSonido)
+}//*--------------------------> tienes un array con la información sobre los usuarios de la cual no puedes acceder a los sonidos
+console.log("🚀 ~ usuarios:", usuarios)
+//*---------------------------> el siguiente paso es acceder a sus sonidos favoritos
+let sonidosFavoritos = []
+
+for (let favoritesSounds of usuarios){
+    if(!sonidosFavoritos.includes(usuarios.favoritesSounds)){
+        sonidosFavoritos.push(usuarios.favoritesSounds)
+    }
+
+}
+console.log("🚀 ~ sonidosFavoritos:", sonidosFavoritos)
+
+
+    //console.log("🚀 ~ claveSonido:", claveSonido)*/
+
+                            //?---------- NECESITAS HACERLO ANIDADO PARA QUE FUNCIONE
+
+let totalSonido = []
+let totalVolumen = []
+
+for (let user of users){                                            //* sacas los usuarios de users
+    for(let sound in user.favoritesSounds){                         //* accedes a los sonidos dentro de favoritesSounds
+    let volume = user.favoritesSounds[sound].volume                 //* estableces que volumen será el volumen de cada 
+    totalVolumen += volume
+    totalSonido ++;
+    }
+}
+
+let mediaVolumen = totalVolumen / totalSonido
+
+console.log(`La media es ${mediaVolumen}`)
+    // For in para sacar las claves, For of para los valores
+
+
+//?----------- Iteración #3: Mix Fors
+//! Dado el siguiente javascript usa forof y forin para saber cuantas veces ha sido cada sonido agregado por los usuarios a favorito. 
+//! Para ello recorre la lista de usuarios y usa forin para recoger el nombre de los sonidos que el usuario tenga como favoritos.
+//! Una vez accedas a ellos piensa en la mejor forma de hacer un conteo de cada vez que ese sonido se repita como favorito en cada usuario.
+
+const users2 = [
+    {name: 'Manolo el del bombo',
+    favoritesSounds: {
+    waves: {format: 'mp3', volume: 50},
+    rain: {format: 'ogg', volume: 60},
+    firecamp: {format: 'mp3', volume: 80},
+    }
+    },
+    {name: 'Mortadelo',
+    favoritesSounds: {
+    waves: {format: 'mp3', volume: 30},
+    shower: {format: 'ogg', volume: 55},
+    train: {format: 'mp3', volume: 60},
+    }
+    },
+    {name: 'Super Lopez',
+    favoritesSounds: {
+    shower: {format: 'mp3', volume: 50},
+    train: {format: 'ogg', volume: 60},
+    firecamp: {format: 'mp3', volume: 80},
+    }
+    },
+    {name: 'El culebra',
+    favoritesSounds: {
+    waves: {format: 'mp3', volume: 67},
+    wind: {format: 'ogg', volume: 35},
+    firecamp: {format: 'mp3', volume: 60},
+    }
+    },
+    ]
+
+let usuarios2 = []
+    
+for (let user of users2){
+    if(!usuarios2.includes(user)){
+    usuarios2.push(user)
+    }
+}//-------------------------------> se han extraido los usuarios
+
+let accSound = []
+
+for (let sound in user){
+    acc = 0
+    if (!accSound.includes(sound)){
+         acc++
+        accSound.push(sound)
+       
+    }
+}
+console.log("🚀 ~ accSound:", accSound)
