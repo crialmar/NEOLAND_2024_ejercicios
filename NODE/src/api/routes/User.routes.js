d6f08ad127a5c2984743279f8d5edde85da6dbc8
crialmar <cristina.almagon@gmail.com>
2024-02-05 17:12:21 +0100

    Password no log

--- NODE/src/api/routes/User.routes.js
+++ NODE/src/api/routes/User.routes.js
@@ -6,7 +6,11 @@ const {
   registerWithRedirect,
   sendCode,
   login,
-  autoLogin
+  autoLogin,
+  resendCode,
+  checkNewUser,
+  changePassword,
+  sendPassword
 } = require("../controllers/User.controllers");
 const express = require("express");
 const UserRoutes = express.Router();
@@ -15,12 +19,19 @@ UserRoutes.post("/registerLargo", upload.single("image"), registerLargo);
 UserRoutes.post("/registerUtil", upload.single("image"), register);
 UserRoutes.get("/register", upload.single("image"), registerWithRedirect);
 
-
+UserRoutes.post("/resend", resendCode);
+UserRoutes.post("/check", checkNewUser);
 
 UserRoutes.post("/login", login);
 UserRoutes.post("/login/autologin", autoLogin)
+
+UserRoutes.patch("/forgotpassword", changePassword);
+UserRoutes.patch("/sendPassword/:id", sendPassword); //?-----> con auth
+
 //* ------------------> rutas que pueden ser redirect
 UserRoutes.get("/register/sendMail/:id", sendCode); //* :id ---> es el nombre del param
+  //? ----> con auth
+
 module.exports = UserRoutes;
 
 
