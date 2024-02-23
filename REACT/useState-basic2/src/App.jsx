f3b59c7f65f8930da3dc43a422bdaeba1af49c49
crialmar <cristina.almagon@gmail.com>
2024-02-23 12:21:03 +0100

    useState

---
+++ REACT/useState-basic2/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { ObjectState } from './components/ObjectState';
+
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <ObjectState />
+    </div>
+  );
+};
+
+
+
+export default App
